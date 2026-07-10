9417b5ff97794511c2524194f35e8b8662041f7f
Mishal-ui <mishifati219@gmail.com>
2026-07-10 15:37:19 +0500

    Added security headers and middleware updates

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,20 @@
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+
+// export function middleware(request: NextRequest) {
+//   if (request.nextUrl.pathname !== "/maintenance") {
+//     return NextResponse.redirect(new URL("/maintenance", request.url));
+//   }
+// }
+
+// export const config = {
+//   matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+// };
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname !== "/maintenance") {
-    return NextResponse.redirect(new URL("/maintenance", request.url));
-  }
+  return NextResponse.next();
 }
 
 export const config = {
